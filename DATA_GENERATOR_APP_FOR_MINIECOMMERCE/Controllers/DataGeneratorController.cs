209a84aa2b543944b69074aaa8eda5f27d3254ed
Waseem1O1 <waseem_dn_intern@software-craft.com>
2022-03-28 16:32:56 +0500

    Add project files.

---
+++ DATA_GENERATOR_APP_FOR_MINIECOMMERCE/Controllers/DataGeneratorController.cs
@@ -0,0 +1,62 @@
+﻿using DATA_GENERATOR_APP_FOR_MINIECOMMERCE.Models;
+using DATA_GENERATOR_APP_FOR_MINIECOMMERCE.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DATA_GENERATOR_APP_FOR_MINIECOMMERCE.Controllers
+{
+    public class DataGeneratorController : Controller
+    {
+        private readonly IDataGenerator<ProductModel> _contactsGeneratorService;
+        public IProductRepository _ProductRepository { get; }
+
+        public DataGeneratorController(IDataGenerator<ProductModel> dataGeneratorService, IProductRepository ProductRepository)
+        {
+            _contactsGeneratorService = dataGeneratorService;
+            _ProductRepository = ProductRepository;
+        }
+        [HttpGet]
+        public ActionResult GenerateData()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> GenerateData(IFormFile image, int noOfRecords)
+        {
+            byte[] img = null;
+            if (image != null)
+            {
+                if (image.Length > 0)
+                {
+                    if(noOfRecords >0)
+                    {
+
+                  
+                    var fileName = Path.GetFileName(image.FileName);
+                    var fileExtension = Path.GetExtension(fileName);
+                    var newFileName = String.Concat(Convert.ToString(Guid.NewGuid()), fileExtension);
+                    using (var target = new MemoryStream())
+                    {
+                        image.CopyTo(target);
+                        img = target.ToArray();
+                    }
+                    var data = _contactsGeneratorService.Collection(noOfRecords);
+                    var product = await _ProductRepository.GenerateProducts(data, img);
+                    ViewBag.success = data.Count + " Products Generated Successfully";
+                    }
+                    else
+                    {
+                        ViewBag.danger1 = " Please Enter Positive Number";
+                    }
+                }
+            }
+            else
+            {
+                ViewBag.danger =  " Please Choose a file";
+
+            }
+
+            return View();
+        }
+    }
+}
